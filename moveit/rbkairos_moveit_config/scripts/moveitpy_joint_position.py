51e48314d4f1bd24de8309a891739634d29ed56b
Dimitrios Kanoulas <dkanoulas@gmail.com>
2020-05-07 15:58:36 +0100

    Trying to make moveit work with kairos

---
+++ moveit/rbkairos_moveit_config/scripts/moveitpy_joint_position.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import rospy
+from moveit_python import MoveGroupInterface
+from moveit_msgs.msg import MoveItErrorCodes
+
+rospy.init_node('moveit_python_tutorial', anonymous=True)
+
+move_group = MoveGroupInterface("arm", "robot_base_link")
+
+joints = ["robot_arm_elbow_joint", "robot_arm_shoulder_lift_joint", "robot_arm_shoulder_pan_joint",
+                  "robot_arm_wrist_1_joint", "robot_arm_wrist_2_joint", "robot_arm_wrist_3_joint"]
+
+pose = [2.32, -2.89, -2.32, -2.01, -1.51, 1.54]
+
+while not rospy.is_shutdown():
+
+    result = move_group.moveToJointPosition(joints, pose, 0.02)
+    if result:
+
+        if result.error_code.val == MoveItErrorCodes.SUCCESS:
+            rospy.loginfo("Trajectory successfully executed!")
+        else:
+            rospy.logerr("Arm goal in state: %s",
+                            move_group.get_move_action().get_state())
+    else:
+        rospy.logerr("MoveIt failure! No result returned.")
+
+move_group.get_move_action().cancel_all_goals()
